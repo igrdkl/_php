d99de33f101fa4d8d4a0528596fb1805f4834f49
IgorDikal <igrdkl@icloud.com>
2021-12-27 20:05:33 +0200

    two commit

---
+++ models/Article.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace models;
+
+class Article extends BaseModel
+{
+    
+    public function view()
+    {
+        //получаем информацию об одной статье
+    }
+
+    public static function getList()
+    {
+
+    }
+}
